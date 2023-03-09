93fe1b421ddc4d098a20c4320c5ed98029d05926
jzvele <jzvelebilova@gmail.com>
2023-03-09 14:14:17 -0500

    a6 lab

---
+++ src/labs/index.js
@@ -0,0 +1,12 @@
+import Nav from "/Users/josie/2023/spring/webdev/tuiter-react-web-app/src/nav.js";
+import Assignment6 from "./a6"
+
+function Labs() {
+    return(
+        <div>
+            <Nav/>
+            <Assignment6/>
+        </div>
+    );
+}
+export default Labs;
