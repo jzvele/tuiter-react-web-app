a4c66425530b996ec910243a53818474ce70db4c
jzvele <jzvelebilova@gmail.com>
2023-03-26 22:00:42 -0400

    A7 Tuiter

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
                   <Route path="/*" element={<Labs/>}/>
                   <Route path="/hello"
                          element={<HelloWorld/>}/>
-                  <Route path="/tuiter"
+                  <Route path="/tuiter/*"
                          element={<Tuiter/>}/>
               </Routes>
           </div>
